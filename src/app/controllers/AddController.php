fb71d12f8db41f26b8542a5233b511a7a1545c38
aakash_vishwakarma <aakashvishwakarma@cedcoss.com>
2022-04-16 13:28:18 +0530

    edit functoinality

--- src/app/controllers/AddController.php
+++ src/app/controllers/AddController.php
@@ -41,21 +41,68 @@ class AddController extends Controller
         }
 
           $collection = $this->mongo->test->task;
-        // $insertOneResult = $collection->insertOne(['username' => $username, 'category' => $category, 'price' => $price, 'stock' => $stock, 'myarray' => $arr]);
-
-        $cursor = $collection->find();
-        $this->view->cursor = $cursor;
+        $insertOneResult = $collection->insertOne(['username' => $username, 'category' => $category, 'price' => $price, 'stock' => $stock, 'myarray' => $arr]);
+        $this->response->redirect('/add/view');
        
     }
 
 
 
 
-    public function tryAction()
+    public function viewAction()
     {
-        // $collection = $this->mongo->test->students;
-        // $insertOneResult = $collection->insertOne(['name' => '12345', 'rollno' => '12345',]);
-        // print_r($insertOneResult->getInsertedId());
-        die();
+        $collection = $this->mongo->test->task;
+        $cursor = $collection->find();
+        $this->view->cursor = $cursor;
     }
+
+    public function editAction()
+    {
+        $id = $_GET['id'];
+        $collection = $this->mongo->test->task;
+        $edit_data = $collection->findOne([ '_id' => new MongoDB\BSON\ObjectId($id)]);
+         echo "<pre>";
+         print_r($edit_data);
+        //die;
+         $this->view->edit_data = $edit_data;
+   
+    }
+
+    public function updateAction()
+    {
+      //  print_r($_POST);
+       
+        $id = $_POST['id'];
+        // print_r($id);
+      //  die;
+         $collection = $this->mongo->test->task;
+        $edit_data = $collection->findOne([ '_id' => new MongoDB\BSON\ObjectId($id)]);
+        $edit_data->username = $this->request->getPost('name');
+        $edit_data->category = $this->request->getPost('category');
+        $edit_data->price = $this->request->getPost('price');
+        $edit_data->stock = $this->request->getPost('stock');
+        // $edit_data->label = $this->request->getPost('label');
+        // $edit_data->value = $this->request->getPost('value');
+ 
+        // $c = $this->request->getPost('label');
+        for($p =0; $p < count($edit_data->myarray); $p++) {
+            $edit_data->myarray[$p]["label"] = $this->request->getPost('label')[$p] ;
+            $edit_data->myarray[$p]["value"] = $this->request->getPost('value')[$p] ;
+            // print_r($this->request->getPost('label')[$p]);
+            // echo "-----------------------";
+            // print_r($this->request->getPost('value')[$p]);   
+            // echo "+++++++++++++++++";
+            // echo $p;
+        }
+           
+        $collection->updateOne([ '_id' => new MongoDB\BSON\ObjectId($id)], ['$set' => $edit_data]);
+        $this->response->redirect('/add/view');
+
+
+
+        // die;
+   
+    }
+
+
 }
